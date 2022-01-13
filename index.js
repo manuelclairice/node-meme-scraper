19a4c11d91ac7e9f9fb25101c2585586354d5dae
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-13 17:06:36 +0100

    Update Readme and change comments in index.js

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ import fetch from 'node-fetch';
 
 async function memescraper() {
   const url = 'https://memegen-link-examples-upleveled.netlify.app/';
-  // if (!url) throw new TypeError('Need to provide an url as first argument.');
   const { body: html } = await got(url);
   const links = htmlUrls({ html, url });
 
-  // Define array in which push all urls
+  // Define array in which all urls will be pushed
   const allUrls = [];
 
   links.forEach((element) => {
     allUrls.push(element.url);
   });
 
-  // Define array of image urls
+  // Define array of only image urls
   const imageUrls = allUrls.filter((element) =>
     element.startsWith('https://api.memegen.link/images'),
   );
 
-  // Loop over the array of image urls and downloads the images
+  // Loop over the array of image urls
   for (let a = 0; a < 10; a++) {
     // Create file for the [a] image
     fs.open(`./memes/0${a + 1}.jpg`, 'w', (err) => {
@@ -29,9 +28,7 @@ async function memescraper() {
         throw err;
       }
     });
-
-    // Downloads the image to this file
-
+    // Downloads the image to the created file
     const imageUrl = imageUrls[a];
 
     fetch(imageUrl)
@@ -43,6 +40,8 @@ async function memescraper() {
       });
   }
 }
+
+// Run the function
 
 memescraper().catch((error) => {
   console.log(error);
