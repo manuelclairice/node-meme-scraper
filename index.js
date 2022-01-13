eb7ec6810e70425df5a94b97aa3dadf10cde6ffe
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-13 15:18:52 +0100

    Remove unecessary code

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// import scrapefrom from 'scrapefrom';
 import got from 'got';
 import htmlUrls from 'html-urls';
-// import downloadImage from 'download-image';
-// import download from 'download-file';
 import fs from 'fs';
 import request from 'request';
 import fetch from 'node-fetch';
 
-(async () => {
+async function memescraper() {
   const url = 'https://memegen-link-examples-upleveled.netlify.app/';
   if (!url) throw new TypeError('Need to provide an url as first argument.');
   const { body: html } = await got(url);
@@ -48,121 +45,6 @@ import fetch from 'node-fetch';
       res.body.pipe(fs.createWriteStream(`./memes/0${a + 1}.jpg`)),
     );
   }
-})();
+}
 
-// const imageUrl =
-//   'https://api.memegen.link/images/ermg/ermahgerd/memes.jpg?width=300';
-
-// fetch(imageUrl).then((res) =>
-//   res.body.pipe(fs.createWriteStream('./memes/01.jpg')),
-// );
-
-// Create file for the [a] image
-// fs.open('./memes/01.jpg', 'w', (err, file) => {
-//   if (err) {
-//     throw err;
-//   }
-
-//   console.log('File is created.');
-// });
-
-// Other Method do download images
-
-// const download = (imageUrl, path, callback) => {
-//   request.head(imageUrl, (err, res, body) => {
-//   request(imageUrl)
-//   .pipe(fs.createWriteStream(path))
-//   .on(‘close’, callback)
-//   })
-//   }
-
-//   const imageUrl = 'https://api.memegen.link/images/ermg/ermahgerd/memes.jpg?width=300'
-//   const path = './memes/'
-
-//   download(imageUrl, path, () => {
-//   console.log(‘✅ Done!’)
-//   })
-
-// Download image and save to meme
-// const url2 =
-//   'https://api.memegen.link/images/ermg/ermahgerd/memes.jpg?width=300';
-
-// const options = {
-//   directory: './memes/',
-//   filename: 'cat.gif',
-// };
-
-// download(url2, options, function (err) {
-//   if (err) throw err;
-//   console.log('Error in downloading the URL');
-// });
-
-// Download an image
-// downloadImage(
-//   'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300',
-//   `./memes`,
-// );
-
-// (async () => {
-//   const url = 'https://memegen-link-examples-upleveled.netlify.app/';
-//   if (!url) throw new TypeError('Need to provide an url as first argument.');
-//   const { body: html } = await got(url);
-//   const links = htmlUrls({ html, url });
-
-//   links.forEach(({ url }) => console.log(url));
-// })();
-
-// let scrapedHTML = [];
-// scrapefrom('https://memegen-link-examples-upleveled.netlify.app/').then((e) => {
-//   // console.log(e.result.html_raw_full);
-//   scrapedHTML.push(e.result.html_raw_full);
-//   console.log(typeof e.result.html_raw_full);
-//   console.log(scrapedHTML);
-// });
-
-// import request from 'request';
-// import cheerio from 'cheerio';
-// const url = 'http://www.html.it/autore/gabroman/';
-// import fs from 'fs';
-
-// const getPage = (cb) => {
-//   request(
-//     url,
-//     {
-//       timeout: 3000,
-//     },
-//     (error, response, body) => {
-//       if (!error) {
-//         cb(body);
-//       }
-//     },
-//   );
-// };
-
-// const savePage = (data) => {
-//   let contents = "'use strict';" + '\n\n';
-//   contents += 'const HTMLItArticles = ';
-//   contents += JSON.stringify(data) + ';\n\n';
-//   contents += 'module.exports = HTMLItArticles;';
-
-//   fs.writeFileSync(test + '/articles.js', contents);
-// };
-
-// const parsePage = (data) => {
-//   const $ = cheerio.load(data);
-//   let output = [];
-//   $('.author-article').each((i, elem) => {
-//     let $a = $(elem).find('a');
-//     let datum = {
-//       title: $a.text(),
-//       url: $a.attr('href'),
-//     };
-//     output.push(datum);
-//   });
-//   return output;
-// };
-
-// getPage((html) => {
-//   let data = parsePage(html);
-//   savePage(data);
-// });
+memescraper();
